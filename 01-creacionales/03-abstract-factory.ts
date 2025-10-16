9ae3f18d92bd69c0cc25c96796fd003f139dbe58
adri11sr <adrysanchezr@gmail.com>
2025-10-16 20:30:59 +0200

    SECTION 6 - ABSTRACT FACTORY

--- 01-creacionales/03-abstract-factory.ts
+++ 01-creacionales/03-abstract-factory.ts
@@ -12,8 +12,86 @@
  * https://refactoring.guru/es/design-patterns/abstract-factory
  */
 
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  *  El propósito del Abstract Factory es crear familias de objetos relacionados
  *  (en este caso, hamburguesas y bebidas) sin especificar las clases concretas
  *  de cada uno de esos objetos en el código principal.
  */
+
+interface Burger {
+    prepare(): void;
+}
+
+interface Drink{
+    pour():void;
+}
+
+class ChickenBurger implements Burger {
+  prepare(): void {
+    console.log("Preparando hamburguesa de %cPollo", COLORS.yellow);
+  }
+}
+
+class BeefBurger implements Burger {
+    prepare(): void {
+        console.log("Preparando hamburguesa de %cVacuno", COLORS.red);
+    }
+}
+
+class Water implements Drink {
+    pour(): void {
+      console.log("Sirviendo un vaso de %cagua",COLORS.blue)
+    }
+}
+
+class Soda implements Drink {
+    pour(): void {
+      console.log("Sirviendo un vaso de %cgaseosa",COLORS.pink)
+    }
+}
+
+interface RestaurantFactory {
+    createBurger(): Burger;
+    createDrink():Drink;
+}
+
+class FastFoodRestaurant implements RestaurantFactory {
+  createBurger(): Burger {
+    return new BeefBurger();
+  }
+  createDrink(): Drink {
+    return new Soda();
+  }
+
+}
+
+class HealthtyRestaurant implements RestaurantFactory {
+  createBurger(): Burger {
+    return new ChickenBurger();
+  }
+  createDrink(): Drink {
+    return new Water();
+  }
+
+}
+
+
+function main(factory: RestaurantFactory){
+
+    const burger = factory.createBurger();
+    const drink = factory.createDrink();
+
+    burger.prepare();
+    drink.pour();
+
+}
+
+console.log("\n%cPedido del menú regular:", COLORS.green)
+    
+main( new FastFoodRestaurant());
+
+console.log("\n\n%cPedido del menú saludable:", COLORS.green)
+    
+main( new HealthtyRestaurant());
