78b9cdcb47e9da7c80b35b69562ebd5bd6931d1b
adri11sr <adrysanchezr@gmail.com>
2025-10-29 09:47:24 +0100

    SECTION 16 - DECORATOR

--- 02-estructurales/04-decorator.ts
+++ 02-estructurales/04-decorator.ts
@@ -11,3 +11,73 @@
  *
  * https://refactoring.guru/es/design-patterns/decorator
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+
+interface Notification {
+    send(message:string):void;
+}
+
+class BasicNotification implements Notification{
+
+  send(message: string): void {
+    console.log(`Enviando notificacion básica: %c${message}`,COLORS.blue);
+  }
+    
+}
+
+// Classe Decoradora
+abstract class NotificationDecortator implements Notification{
+
+    protected notification: Notification;
+
+    constructor(notification:Notification){
+        this.notification = notification;
+    }
+
+  send(message: string): void {
+    this.notification.send(message);
+  }
+    
+}
+
+// Crear diferentes decoradores
+class EmailDecorator extends NotificationDecortator {
+
+    private sendEmail(message:string){
+        console.log(`Enviar notificaión por email: %c${message}`,COLORS.green)
+    }
+
+    override send(message: string): void {
+      super.send(message);
+      this.sendEmail(message);
+    }
+
+}
+
+class SMSDecorator extends NotificationDecortator {
+
+    private sendSMS(message:string){
+        console.log(`Enviar notificaión por SMS: %c${message}`,COLORS.yellow)
+    }
+
+    override send(message: string): void {
+      super.send(message);
+      this.sendSMS(message);
+    }
+
+}
+
+function main(){
+
+    let notificacion:Notification = new BasicNotification();
+
+    notificacion = new EmailDecorator(notificacion);
+    notificacion = new SMSDecorator(notificacion);
+
+    notificacion.send('Alerta del sistema');
+
+}
+
+main();
