9296659c989d20ff224e16b56ad8512f8549fb09
adri11sr <adrysanchezr@gmail.com>
2025-10-29 09:15:01 +0100

    SECTION 15 - COMPOSITE

--- 02-estructurales/03.2-composite.ts
+++ 02-estructurales/03.2-composite.ts
@@ -44,19 +44,28 @@ class MenuItem implements MenuComponent {
 // 3. Clase MenuCategory
 // Representa una categoría de menú que puede contener otros ítems o subcategorías.
 class MenuCategory implements MenuComponent {
-  // TODO: Crear dos propiedades privadas: name y items
-  // Name sting y items arreglo de MenuComponent
-  // Name es recibida en el constructor, items se inicializa como un arreglo vacío
 
-  //TODO: Sobrecarga de operadores - Item puede ser MenuComponent o un arreglo de MenuComponent
-  add(item: unknown): void {
-    // TODO: Implementar la sobrecarga de operadores
-    throw new Error('Method not implemented.');
+  private items:MenuComponent[] = [];
+  private name:string;
+
+  constructor(name:string){
+    this.name = name;
+  }
+
+  add(item: MenuComponent | MenuComponent[]): void {
+    
+    if (Array.isArray(item)){
+      this.items.push(...item);
+      return;
+    }
+
+    this.items.push(item);
+    
   }
 
   showDetails(indent: string = ''): void {
     console.log(`%c${indent}+ ${this.name}`, COLORS.blue);
-    // TODO: Implementar foreach
+    this.items.forEach(component => component.showDetails(indent + ' '));
   }
 }
 
@@ -71,6 +80,7 @@ function main() {
   const soda = new MenuItem('Refresco', 2.5);
   const dessert = new MenuItem('Pastel de chocolate', 6.5);
   const coffee = new MenuItem('Café', 1.99);
+  const te = new MenuItem('Te', 0.99);
 
   // Crear categorías de menú y añadir ítems
   const appetizers = new MenuCategory('Entradas');
@@ -81,8 +91,16 @@ function main() {
   mainCourse.add(steak);
 
   const beverages = new MenuCategory('Bebidas');
-  beverages.add(soda);
-  beverages.add(coffee);
+
+  const hotBeverages = new MenuCategory('Calientes');
+  const coldBeverages = new MenuCategory('Frías');
+
+  coldBeverages.add(soda);
+
+  hotBeverages.add(coffee);
+  hotBeverages.add(te);
+
+  beverages.add([coldBeverages, hotBeverages]);
 
   const desserts = new MenuCategory('Postres');
   desserts.add(dessert);
